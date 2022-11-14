2359b358963e820a41819c7bcf9215171eb053c4
ClayScarboro <clayscarbor@gmail.com>
2022-11-14 15:11:36 -0500

    Update cache.cc

--- src/cache.cc
+++ src/cache.cc
@@ -100,7 +100,7 @@ cacheLine * Cache::findLine(ulong addr)
 }
 
 /*upgrade LRU line to be MRU line*/
-void Cache::updateLRU(cacheLine *line)
+void Cache::updateLRU(cacheLine *line) d
 {
    line->setSeq(currentCycle);  
 }
