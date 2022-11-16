1e354746892701669bf88d762acb24396136d1f2
ClayScarboro <clayscarbor@gmail.com>
2022-11-16 11:03:19 -0500

    Update cache.cc

--- src/cache.cc
+++ src/cache.cc
@@ -90,8 +90,10 @@ int Cache::Access(ulong addr,uchar op)
       if(op == 'w') line->setFlags(MODIFIED);
    }
    
+   printf("DEbug2d\n");
    cacheLine * line2 = findLine(addr);
    line2->isShared();
+   printf("DEbug3d\n");
    currentTransaction = doMsiReq(addr,currentTransaction);
    
    if (currentTransaction == 2) BusRdX++;
